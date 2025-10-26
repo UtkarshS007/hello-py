1b73130701c9d71e4a06a4c7829faf5ca9819e72
Utkarsh Singh <75792507+UtkarshS007@users.noreply.github.com>
2025-10-26 15:17:22 +0530

    Changes made to ecomm_trials for adding a PATH SHIM - helping in making the repo portable.

--- scripts/ecomm_trials.py
+++ scripts/ecomm_trials.py
@@ -3,7 +3,10 @@ This file helps in running multiple trials of the ecommerce task
 prompts that we had set.
 Each trial uses a new random 1000-row sample for authenticity.
 """
-
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+#The above "Path Shim" helps make the repo importable no matter where this file 
+#will be run from 
 import pandas as pd
 from tasks.ecomm_preprocess import sampling_taskdata, describe_task
 from tools.ecomm_tools import preprocess
